2ba717a911e772f3cb2552bdf23267559f755643
PolymagicYang <PolymagicYang@protonmail.com>
2022-02-28 15:16:36 +0800

    corret bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 curl https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init --output rustup-init
 chmod +x rustup-init
-./rustup-init -y & rm ./rustup-init
+./rustup-init -y && rm ./rustup-init
 source $HOME/.cargo/env
