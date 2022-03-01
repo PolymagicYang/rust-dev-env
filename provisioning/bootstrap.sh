46ee9a5ddeb53de0cf0969724fb3df0ee835d4c5
PolymagicYang <PolymagicYang@protonmail.com>
2022-03-01 15:55:55 +0800

    add workspace directory as rust project container

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -2,15 +2,13 @@
 apt-get update -y
 
 # add linker(cc) environment.
-apt install build-essential
+apt-get install build-essential
 
 # rust nightly installation.
 wget https://static.rust-lang.org/dist/rust-nightly-x86_64-unknown-linux-gnu.tar.gz
-tar -zxvf rust-nightly-x86_64-unknown-linux-gnu.tar.gz && cd ./rust-nightly-x86_64-unknown-linux-gnu
-./install.sh
-source $HOME/.cargo/env
+tar -zxvf rust-nightly-x86_64-unknown-linux-gnu.tar.gz && cd ./rust-nightly-x86_64-unknown-linux-gnu && \
+./install.sh && cd ../ && rm -r rust-nightly-x86_64-unknown-linux-gnu.tar.gz
 
 # rust toolchains installation.
 
-# rust-analyzer:
-
+echo "-------- booting finish! ----------"
